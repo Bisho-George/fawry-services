6e9c64bd5c5d97d7073d261fee83f6accfeaa867
seif mahmoud <seifmahmoud@seifs-MacBook-Pro.local>
2023-01-01 16:33:14 +0200

    Adding walletController

---
+++ src/main/java/com/example/phase/payment/WalletController.java
@@ -0,0 +1,39 @@
+package com.example.phase.payment;
+
+import com.example.phase.transaction.Transaction;
+import com.example.phase.transaction.TransactionDatabase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/Wallet")
+public class WalletController {
+    private Wallet w;
+    private TransactionDatabase tb;
+
+    @Autowired
+    public WalletController(Wallet w,TransactionDatabase tb) {
+        this.w = w;
+        this.tb=tb;
+    }
+
+
+    @PostMapping("/Add/{amount}")
+    public String addToWallet(@PathVariable ("amount") double amount){
+        w.addToWallet(amount);
+        int id = tb.getAll().size()+1;
+        tb.addAddition(id,amount);
+        return amount + " Added Successfully To Your Wallet";
+
+
+    }
+
+    @GetMapping("/ViewAmount")
+    public String viewAmount(){
+        return "Your Current Balance = " + w.getAmount();
+
+    }
+
+
+
+}
